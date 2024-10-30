9bc9a8c1b89b3992302338cdc08d606fec51fdc5
seantesterman <seantesterman@gmail.com>
2024-10-29 23:33:43 -0600

    1.5 Fileservers

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	server := http.Server{Addr: ":8080", Handler: mux}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal("Listen And Serve failure")

23dff4a79d3a874c5b2e2bd176ae1aeb9b35da03
seantesterman <seantesterman@gmail.com>
2024-11-04 17:24:51 -0700

    2.1 Stateful Handlers

--- main.go
+++ main.go
@@ -1,18 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	config := apiConfig{}
 	mux := http.NewServeMux()
-	server := http.Server{Addr: ":8080", Handler: mux}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", config.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/metrics", config.handlerRequestsCount())
+	mux.Handle("/reset", config.handlerRequestsReset())
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Listen And Serve failure")
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerRequestsCount() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hitCount := cfg.fileserverHits.Load()
+		responseText := fmt.Sprintf("Hits: %d", hitCount)
+		w.Write([]byte(responseText))
+	})
+}
+
+func (cfg *apiConfig) handlerRequestsReset() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Counter reset"))
+	})
 }
